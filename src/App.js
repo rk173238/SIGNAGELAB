4fc0ea8be935176fa75e9b684fe3f21463d256dc
Rohit <Rohit@CRS-IS-SMC.smc.smc.com>
2020-10-30 03:44:13 +0000

    initial commit

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+import './App.css';
+// import GridCard from './check/GridCard';
+// import Icon from './check/Icon';
+// import Tablee from './check/Tablee';
+// import Cardd from './check/Cardd';
+// import Homepage from './Homepage/Homepage';
+
+
+function App() {
+  return (
+      <div className="App">
+        
+        {/* <Homepage></Homepage> */}
+      </div>
+  );
+}
+
+export default App;
